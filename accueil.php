6a9d33cbf23d182a9b37a92fc9c945db5422a494
ABDELOUALI Khadija <Khadija.Abdelouali@bordeaux-inp.fr>
2019-06-04 00:01:12 +0200

    page d'acceuil 1ère ébauche

---
+++ accueil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="utf-8">
+  <head>
+  <body class="container"> 
+    <button type="button" class="btn btn-outline-danger">Déconnexion</button>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"><center>Accueil</center></h1>
+        <p class="lead"><center>Vous pouvez accéder à vos informations personnelles d'ici.</center></p>
+      </div>
+    </div>
+    <!-- <div class="row"> -->
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">État civil : </h5>
+            <p class="card-text">Nom : </p>
+            <p class="card-text">Prénom : </p>
+            <p class="card-text">Âge :</p>
+            <p class="card-text">Situation :</p>
+            <a href="#" class="btn btn-primary">Extrait de naissance</a>
+          </div>
+        </div>
+      </div>
+      <a href="#" class="btn btn-primary">Nouvelle déclaration</a>
+  </body>    
