066a52a14c28c733973b37402d596bc779e3e30b
ABDELOUALI Khadija <Khadija.Abdelouali@bordeaux-inp.fr>
2019-06-10 01:31:16 +0200

    mise en forme de la page d'accueil

--- accueil.php
+++ accueil.php
@@ -4,9 +4,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="utf-8">
   <head>
-  <body class="container"> 
+  <body class="container" > 
     <button type="button" class="btn btn-outline-danger">Déconnexion</button>
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron jumbotron-fluid" style="background-color: #800080;">
       <div class="container">
         <h1 class="display-4"><center>Accueil</center></h1>
         <p class="lead"><center>Vous pouvez accéder à vos informations personnelles d'ici.</center></p>
@@ -15,7 +15,7 @@
     <!-- <div class="row"> -->
       <div class="col-sm-6">
         <div class="card">
-          <div class="card-body">
+          <div class="card-body" style="background-image: linear-gradient(to top, #C0C0C0, #800080);">
             <h5 class="card-title">État civil : </h5>
             <p class="card-text">Nom : </p>
             <p class="card-text">Prénom : </p>
--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 // Database configuration
 $GLOBALS['dbServ'] = 'localhost';
 $GLOBALS['dbUser'] = 'root';
-$GLOBALS['dbPass'] = 'root';
+$GLOBALS['dbPass'] = '123456';
 $GLOBALS['dbName'] = 'NEMS';
 
 ?>
